426651b6c1254aafb0f518b9e6837fe9a807bd97
ellenmcintyre123 <g00389482@atu.ie>
2023-01-30 12:05:30 +0000

    complete

--- src/main/java/ie/atu/arrays/ArraysDemo.java
+++ src/main/java/ie/atu/arrays/ArraysDemo.java
@@ -19,14 +19,15 @@ public class ArraysDemo {
         anArray[8] = 900;
         anArray[9] = 1000;
 
-        System.out.println("Element at index 0: " +anArray[0]);
-        System.out.println("Element at index 1: " +anArray[1]);
-        System.out.println("Element at index 2: " +anArray[2]);
-        System.out.println("Element at index 3: " +anArray[3]);
-        System.out.println("Element at index 4: " +anArray[4]);
-        System.out.println("Element at index 5: " +anArray[5]);
-        System.out.println("Elemen at index 6: " +anArray[6]);
-        System.out.println("Element at index 7: " +anArray[7]);
-        System.out.println("Element at index 8: " +anArray[8]);
-        System.out.println("Element at index 9: " +anArray[9]);
+        System.out.println("Element at index 0: " + anArray[0]);
+        System.out.println("Element at index 1: " + anArray[1]);
+        System.out.println("Element at index 2: " + anArray[2]);
+        System.out.println("Element at index 3: " + anArray[3]);
+        System.out.println("Element at index 4: " + anArray[4]);
+        System.out.println("Element at index 5: " + anArray[5]);
+        System.out.println("Elemen at index 6: " + anArray[6]);
+        System.out.println("Element at index 7: " + anArray[7]);
+        System.out.println("Element at index 8: " + anArray[8]);
+        System.out.println("Element at index 9: " + anArray[9]);
+    }
 }
