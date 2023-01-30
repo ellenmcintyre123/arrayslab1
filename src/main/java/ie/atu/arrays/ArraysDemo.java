612eadc5ec4898d1c07da99caba0a826e563e7ed
ellenmcintyre123 <g00389482@atu.ie>
2023-01-30 12:32:43 +0000

    out of bounds message

--- src/main/java/ie/atu/arrays/ArraysDemo.java
+++ src/main/java/ie/atu/arrays/ArraysDemo.java
@@ -1,5 +1,7 @@
 package ie.atu.arrays;
 
+import java.util.Scanner;
+
 public class ArraysDemo {
 
     public static void main(String[] args) {
@@ -19,15 +21,22 @@ public class ArraysDemo {
         anArray[8] = 900;
         anArray[9] = 1000;
 
-        System.out.println("Element at index 0: " + anArray[0]);
-        System.out.println("Element at index 1: " + anArray[1]);
-        System.out.println("Element at index 2: " + anArray[2]);
-        System.out.println("Element at index 3: " + anArray[3]);
-        System.out.println("Element at index 4: " + anArray[4]);
-        System.out.println("Element at index 5: " + anArray[5]);
-        System.out.println("Elemen at index 6: " + anArray[6]);
-        System.out.println("Element at index 7: " + anArray[7]);
-        System.out.println("Element at index 8: " + anArray[8]);
-        System.out.println("Element at index 9: " + anArray[9]);
+        int indexValue = 0;
+
+        for(int value: anArray){
+            System.out.println("Element at index " + indexValue + " :" + value);
+            indexValue++;
+        }
+
+
+        Scanner myScan = new Scanner(System.in);
+         try{
+             System.out.println("please enter a number: ");
+             anArray[10] = myScan.nextInt();
+         }
+         catch(ArrayIndexOutOfBoundsException aiob)
+         {
+             System.out.println("went outside memory block");
+         }
     }
 }
